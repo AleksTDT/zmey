da0d4916e0389d9e446561cd027f047eee6a868a
Aleks <eng.a.todorov@gmail.com>
2022-06-19 21:35:31 +0300

    init solidjs

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <>Hi mom!</>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import App from './App';
+/* @refresh reload */
+import { render } from 'solid-js/web';
+
+render(() => <App />, document.getElementById('root') as HTMLElement);
