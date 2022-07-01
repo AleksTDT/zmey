21eeb4777c6127221999930df01f845806055275
Aleks <eng.a.todorov@gmail.com>
2022-07-02 02:04:49 +0300

    Added code viewer

---
+++ src/components/CodeViewer.tsx
@@ -0,0 +1,19 @@
+import { Component } from 'solid-js';
+import Prism from 'prismjs';
+
+interface CodeViewerProp {
+    code: string;
+}
+
+const CodeViewer: Component<CodeViewerProp> = (prop: CodeViewerProp) => {
+    
+    const highlightedHtml: string = Prism.highlight(prop.code, Prism.languages.javascript, 'javascript');
+
+    return (
+        <pre>
+            <code innerHTML={highlightedHtml}></code>
+        </pre>
+    )
+}
+
+export default CodeViewer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
+import CodeViewer from "../components/CodeViewer";
 import { Component } from "solid-js/types/render/component";
 
 const Home: Component<{}> = (props) => {
     return <>
-        Hi mom!
-        <a href="">Google test</a>
+        <CodeViewer code={`function someFunc() {
+    // this is coment
+    const someVar = \`\${new Date().toLocaleDateString()} is now!\`;
+    let someStr  = 'This is cool'
+    for (let index = 1; index < 100; index++) {
+        console.log(index + index);        
+    }
+
+    /*this isFinitemulty
+    line
+    comment*/
+}`} />
     </>;
 };
 
